1bbc98a4c09f2751e5745c4f50d20b6499cc3228
roccoaltieri0603-oss <roccoaltieri0603@gmail.com>
2026-05-20 21:27:07 -0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public TextMeshProUGUI textoObjetos;
+    public TextMeshProUGUI textoTiempo;
+    public TextMeshProUGUI textoMensaje;
+
+    public float tiempoInicial = 60f;
+
+    private float tiempoActual;
+    private int objetosAgarrados = 0;
+    private int totalObjetos;
+    private bool juegoTerminado = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        tiempoActual = tiempoInicial;
+
+        totalObjetos = FindObjectsOfType<Pickup>().Length;
+
+        if (textoMensaje != null)
+        {
+            textoMensaje.text = "";
+        }
+
+        ActualizarUI();
+    }
+
+    private void Update()
+    {
+        // Reiniciar con R, incluso si ya ganaste o perdiste
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReiniciarEscena();
+        }
+
+        // Si el juego terminó, no sigue bajando el tiempo
+        if (juegoTerminado == true)
+        {
+            return;
+        }
+
+        // Bajar el tiempo
+        tiempoActual = tiempoActual - Time.deltaTime;
+
+        // Perder si llega a 0
+        if (tiempoActual <= 0)
+        {
+            tiempoActual = 0;
+            Perder();
+        }
+
+        ActualizarUI();
+    }
+
+    public void AgarrarObjeto()
+    {
+        if (juegoTerminado == true)
+        {
+            return;
+        }
+
+        objetosAgarrados++;
+
+        // Ganar si agarraste todos los objetos
+        if (objetosAgarrados >= totalObjetos)
+        {
+            Ganar();
+        }
+
+        ActualizarUI();
+    }
+
+    private void ActualizarUI()
+    {
+        if (textoObjetos != null)
+        {
+            textoObjetos.text = "Objetos: " + objetosAgarrados + " / " + totalObjetos;
+        }
+
+        if (textoTiempo != null)
+        {
+            textoTiempo.text = "Tiempo: " + FormatearTiempo(tiempoActual);
+        }
+    }
+
+    private string FormatearTiempo(float tiempo)
+    {
+        int segundos = Mathf.FloorToInt(tiempo);
+        int centesimas = Mathf.FloorToInt((tiempo - segundos) * 100);
+
+        return segundos.ToString("00") + ":" + centesimas.ToString("00");
+    }
+
+    private void Ganar()
+    {
+        juegoTerminado = true;
+
+        if (textoMensaje != null)
+        {
+            textoMensaje.text = "¡Ganaste!\nPresiona R para reiniciar";
+        }
+    }
+
+    private void Perder()
+    {
+        juegoTerminado = true;
+
+        if (textoMensaje != null)
+        {
+            textoMensaje.text = "Perdiste\nPresiona R para reiniciar";
+        }
+    }
+
+    private void ReiniciarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public string playerTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            GameManager.instance.AgarrarObjeto();
+            Destroy(gameObject);
+        }
+    }
+}
